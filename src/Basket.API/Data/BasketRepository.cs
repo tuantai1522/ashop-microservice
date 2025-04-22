b735faeb9e0ddb9d909c5df49d0a460a8db65569
tuantai0708 <tai.bang157@gmail.com>
2025-04-22 21:38:39 +0700

    feat(basket): To add repository for Basket entity

---
+++ src/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,34 @@
+using Basket.API.Entities;
+using Marten;
+
+namespace Basket.API.Data;
+
+public class BasketRepository(IDocumentSession session) : IBasketRepository
+{
+    private readonly IDocumentSession _session = session;
+    
+    public async Task<ShoppingCart?> GetBasketById(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _session.LoadAsync<ShoppingCart>(id, cancellationToken);
+    }
+
+    public async Task StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        _session.Store(basket);
+            
+        await _session.SaveChangesAsync(cancellationToken);
+    }
+
+    public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteBasketById(Guid id, CancellationToken cancellationToken = default)
+    {
+        _session.Delete(id);
+            
+        await _session.SaveChangesAsync(cancellationToken);
+        
+    }
+}
