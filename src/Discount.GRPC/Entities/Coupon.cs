221a43d708e4eca0df57ff4743110c026f51632b
tuantai0708 <tai.bang157@gmail.com>
2025-05-03 11:35:03 +0700

    build(discount-service): To initialize Discount service project

---
+++ src/Discount.GRPC/Entities/Coupon.cs
@@ -0,0 +1,21 @@
+namespace Discount.GRPC.Entities;
+
+public sealed class Coupon
+{
+    /// <summary>
+    /// ID of Coupon
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// ProductID from Catalog Service
+    /// </summary>
+    public Guid ProductId { get; set; }
+    
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Amount which this coupon can be applied to the product
+    /// </summary>
+    public double Amount { get; set; }
+}
